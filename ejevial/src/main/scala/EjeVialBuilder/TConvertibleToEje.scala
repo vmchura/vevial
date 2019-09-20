916165290c7d2e1d55924952fd31528f3f6b5a15
vmchura <vmchura@gmail.com>
2019-09-20 21:33:48 +0000

    Added option to include errors

--- ejevial/src/main/scala/EjeVialBuilder/TConvertibleToEje.scala
+++ ejevial/src/main/scala/EjeVialBuilder/TConvertibleToEje.scala
@@ -1,26 +1,32 @@
 package EjeVialBuilder
 
-import PlanarGeometric.BasicEje.{EfficientSeqEjeElements, TEfficientSeqEjeElements}
+import PlanarGeometric.BasicEje.EfficientSeqEjeElements
 import PlanarGeometric.ProgresiveEje.EfficientEjeProgresiva
 import PlanarGeometric.RestrictiveEje.{EjeEfficientWithRestrictions, ProgresivePoint}
 import com.typesafe.scalalogging.Logger
 
 trait TConvertibleToEje {
-  final def toEje(): EfficientEjeProgresiva = {
-    val sequenceWithRestrictions = EjeEfficientWithRestrictions(getSequenceElements())
-    val logger = Logger(classOf[TConvertibleToEje])
+  final def toEje: Either[Seq[Exception],EfficientEjeProgresiva] = {
 
-    val pointsProgresive =
-      getSequenceProgresivePoint().foldLeft(sequenceWithRestrictions){case (sr, pp) => sr.addRestriction(pp) match {
-        case Right(value) => value
-        case Left(value) => {
-          logger.info(s"$pp was not added as restriction")
-          value
-        }
-      }}
 
-    EfficientEjeProgresiva(pointsProgresive)
-  }
-  protected def getSequenceElements(): EfficientSeqEjeElements
-  protected def getSequenceProgresivePoint(): Iterable[ProgresivePoint]
+    getSequenceElements.map{ efficientSeqElements =>
+
+        val sequenceWithRestrictions = EjeEfficientWithRestrictions(efficientSeqElements)
+        val logger = Logger(classOf[TConvertibleToEje])
+
+        val pointsProgresive =
+          getSequenceProgresivePoint.foldLeft(sequenceWithRestrictions){case (sr, pp) => sr.addRestriction(pp) match {
+            case Right(value) => value
+            case Left(value) =>
+              logger.info(s"$pp was not added as restriction")
+              value
+          }}
+
+        EfficientEjeProgresiva(pointsProgresive)
+      }
+    }
+
+
+  protected def getSequenceElements: Either[Seq[Exception],EfficientSeqEjeElements]
+  protected def getSequenceProgresivePoint: Iterable[ProgresivePoint]
 }
