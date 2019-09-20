b1f1d7e3a1298fb779cb9ea5461fe74962fd2f5f
vmchura <vmchura@gmail.com>
2019-09-20 21:34:38 +0000

    LandXML Importer created

---
+++ ejevial/src/test/scala/EjeVialBuilder/LandXMLToEjeTest.scala
@@ -0,0 +1,28 @@
+package EjeVialBuilder
+
+
+
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.io.Codec
+import scala.reflect.io.File
+
+class LandXMLToEjeTest extends FlatSpec with Matchers{
+
+  behavior of "Creation and build of XMLToEje"
+
+  it should "build correctly" in {
+    val file = File("/home/vmchura/Documents/001.Projects/Vevial/ejevialview/src/main/resources/testTramo.xml")
+    val xmlBuiler = new LandXMLToEje(file.reader(Codec("UTF-8")))
+    val result = xmlBuiler.toEje
+    result match {
+      case Right(value) =>
+        assertResult(6)(value.elements.length)
+        value.length should equal (189.674438876884 +- 1e-3)
+        value.minProg should equal (66725.000000000175 +- 1e-3)
+        value.maxProg should equal ((66725.000000000175+189.674438876884) +- 1e-3)
+
+      case Left(errros) => errros.foreach(println)
+    }
+  }
+}
