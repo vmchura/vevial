4b8eb689151b4253e19a55dd6ec0c1c2c2b83e1a
vmchura <vmchura@gmail.com>
2019-09-20 21:36:40 +0000

    Changed to new defnitions of TConvertibleToEje

--- ejevial/src/main/scala/EjeVialBuilder/SimpleConvertibleToEje.scala
+++ ejevial/src/main/scala/EjeVialBuilder/SimpleConvertibleToEje.scala
@@ -5,12 +5,13 @@ import PlanarGeometric.EjeElement.{CircleSegment, RectSegment}
 import PlanarGeometric.RestrictiveEje.ProgresivePoint
 
 sealed class SimpleConvertibleToEje() extends TConvertibleToEje {
-  override protected def getSequenceElements(): EfficientSeqEjeElements = {
+
+  override protected def getSequenceElements: Either[Seq[Exception], EfficientSeqEjeElements] = {
     val r = RectSegment(Point(100,0),Point(200,0))
     val c = CircleSegment(Point(200,0),Point(200,100),Point(200,200),antiClockWise = true)
-    EfficientSeqEjeElements(TSeqEjeElementsBase().append(r).append(c))
+    Right(EfficientSeqEjeElements(TSeqEjeElementsBase().append(r).append(c)))
   }
 
-  override protected def getSequenceProgresivePoint(): Iterable[ProgresivePoint] = List(new ProgresivePoint(Point(150,0),50),
+  override protected def getSequenceProgresivePoint: Iterable[ProgresivePoint] = List(new ProgresivePoint(Point(150,0),50),
     new ProgresivePoint(Point(200,0),100))
 }
