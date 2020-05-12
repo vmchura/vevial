0d358e99a8c5f10ecea703ffa81975adb86e41c7
vmchura <vmchura@gmail.com>
2020-05-12 21:35:14 +0000

    Added the possibility to calc average value between uncertain values

--- relevamientodata/src/main/scala/io/vmchura/vevial/elementdata/UncertainData.scala
+++ relevamientodata/src/main/scala/io/vmchura/vevial/elementdata/UncertainData.scala
@@ -1,6 +1,6 @@
 package io.vmchura.vevial.elementdata
 
-import io.vmchura.vevial.PlanarGeometric.BasicGeometry.{PlanarVector, Point, TDirection, TPoint}
+import io.vmchura.vevial.PlanarGeometric.BasicGeometry.{PlanarVector, Point, TDirection}
 
 /**
   * https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Normal_Distribution_PDF.svg/1920px-Normal_Distribution_PDF.svg.png
@@ -9,18 +9,52 @@ import io.vmchura.vevial.PlanarGeometric.BasicGeometry.{PlanarVector, Point, TDi
  *
   * @tparam A: type of data
   */
-sealed trait UncertainData[A] {
+sealed trait UncertainData[A,B] { self: B =>
   def value: A
   def sigma2: Double
+  def |-| (other: B): B
 }
 
+object UncertainData{
+  def merge(x: UDouble,y : UDouble): UDouble = {
+    (x.sigma2,y.sigma2) match {
+      case (Double.PositiveInfinity,_) => y
+      case (_,Double.PositiveInfinity) => x
+      case (sx,sy) =>
+        val u = (x.value*sy + y.value*sx)/(sx+sy)
+        UDouble(u,sx+sy)
+    }
+  }
+}
 
+case class UPoint(value: Point, sigma2: Double) extends UncertainData[Point,UPoint] {
+  override def |-|(other: UPoint): UPoint = {
+    val x = UDouble(value.x,sigma2) |-| UDouble(other.value.x,other.sigma2)
+    val y = UDouble(value.y,sigma2) |-| UDouble(other.value.y,other.sigma2)
+    val p = Point(x.value,y.value)
+    UPoint(p,x.sigma2)
+  }
+}
 
-case class UPoint(value: Point, sigma2: Double) extends UncertainData[Point]
+case class UDouble(value: Double, sigma2: Double) extends  UncertainData[Double,UDouble] {
+  override def |-|(other: UDouble): UDouble = UncertainData.merge(this,other)
+}
 
-case class UDouble(value: Double, sigma2: Double) extends  UncertainData[Double]
+case class UDirection(value: TDirection, sigma2: Double) extends  UncertainData[TDirection,UDirection] {
+  override def |-|(other: UDirection): UDirection =  {
+    val dx = UDouble(value.dx,sigma2) |-| UDouble(other.value.dx,other.sigma2)
+    val dy = UDouble(value.dy,sigma2) |-| UDouble(other.value.dy,other.sigma2)
+    val p = TDirection(dx.value,dy.value)
+    UDirection(p,dx.sigma2)
+  }
+}
 
-case class UDirection(value: TDirection, sigma2: Double) extends  UncertainData[TDirection]
+case class UPlanarVector(value: PlanarVector, sigma2: Double) extends UncertainData[PlanarVector,UPlanarVector] {
+  override def |-|(other: UPlanarVector): UPlanarVector = {
+    val d = UDirection(value.direction,sigma2) |-| UDirection(other.value.direction,other.sigma2)
+    val m = UDouble(value.magnitude,sigma2) |-| UDouble(other.value.magnitude,other.sigma2)
 
-case class UPlanarVector(value: PlanarVector, sigma2: Double) extends UncertainData[PlanarVector]
+    UPlanarVector(PlanarVector(d.value,m.value),d.sigma2)
+  }
+}
 
