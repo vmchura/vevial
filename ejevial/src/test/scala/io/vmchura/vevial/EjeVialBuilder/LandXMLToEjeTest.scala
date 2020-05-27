3d34d95d55fda3e31216d4ef194ed973ca9b0265
vmchura <vmchura@gmail.com>
2020-05-27 09:02:06 +0000

    Updated test, Seq[error] => error

--- ejevial/src/test/scala/io/vmchura/vevial/EjeVialBuilder/LandXMLToEjeTest.scala
+++ ejevial/src/test/scala/io/vmchura/vevial/EjeVialBuilder/LandXMLToEjeTest.scala
@@ -2,12 +2,13 @@ package io.vmchura.vevial.EjeVialBuilder
 
 
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.flatspec.AnyFlatSpec
 
 import scala.io.Codec
 import scala.reflect.io.File
 
-class LandXMLToEjeTest extends FlatSpec with Matchers{
+class LandXMLToEjeTest extends AnyFlatSpec with Matchers{
 
   behavior of "Creation and build of XMLToEje"
 
@@ -22,7 +23,7 @@ class LandXMLToEjeTest extends FlatSpec with Matchers{
         value.minProg should equal (66725.000000000175 +- 1e-3)
         value.maxProg should equal ((66725.000000000175+189.674438876884) +- 1e-3)
 
-      case Left(errros) => errros.foreach(println)
+      case Left(errros) => println(errros)
     }
   }
 }
