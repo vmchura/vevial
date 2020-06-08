b6a8dd704191efd9a8dd904afb517ede22915d4d
vmchura <vmchura@gmail.com>
2020-06-08 18:38:34 +0000

    Added a generic OBS type

--- tesisanalytics/src/main/scala/environment/BaseEnvironmentTyped.scala
+++ tesisanalytics/src/main/scala/environment/BaseEnvironmentTyped.scala
@@ -1,8 +1,8 @@
 package environment
 
-import environment.BaseEnvironmentTyped.{Action, EnvironmentError, Observation, RewardFeatureState, TerminalState}
+import environment.BaseEnvironmentTyped.{Action, EnvironmentError, RewardFeatureState}
 
-trait BaseEnvironmentTyped {
+trait BaseEnvironmentTyped[OBS] {
 
   /**
     * The first method called when the experiment starts, called before the
@@ -10,7 +10,7 @@ trait BaseEnvironmentTyped {
     *
     * @return The first state observation from the environment.
     */
-  def env_start(): Observation
+  def env_start(): OBS
 
   /**
     * A step taken by the environment.
@@ -19,20 +19,19 @@ trait BaseEnvironmentTyped {
     * @return (float, state, Boolean): a tuple of the reward, state observation,
     *         and boolean indicating if it's terminal.
     */
-  def env_step(action: Action): Either[EnvironmentError,RewardFeatureState]
+  def env_step(action: Action): Either[EnvironmentError,RewardFeatureState[OBS]]
 
 
 }
 object BaseEnvironmentTyped {
   type Reward = Float
-  type Observation = breeze.linalg.Vector[Double]
   type Action = Int
   object TerminalState extends Enumeration {
     val Finished: TerminalState.Value = Value
     val OnProcess: TerminalState.Value = Value
     val UnknowState: TerminalState.Value = Value
   }
-  case class RewardFeatureState(reward: Reward,observation: Observation,terminalState: TerminalState.Value)
+  case class RewardFeatureState[OBS](reward: Reward,observation: OBS,terminalState: TerminalState.Value)
   sealed trait EnvironmentError
   case class InvalidAction(msg: String) extends EnvironmentError
   object StateIsTerminal extends EnvironmentError
