4731dffcc086c1cb4cdc274cbaccfd0a93fd01ad
vmchura <vmchura@gmail.com>
2021-06-16 09:39:55 -0500

    Update to API of COLOR

--- ejevialview/src/main/scala/HelloScalaFX.scala
+++ ejevialview/src/main/scala/HelloScalaFX.scala
@@ -1,5 +1,5 @@
 import io.vmchura.vevial.EjeVialBuilder.LandXMLToEje
-import Layers.{EjeVialLayer, MilestoneLayer, ObservableListDelegate, ProjectionIRIRelevamientoLayer, SimpleIRIRelevamientoLayer}
+import Layers.{EjeVialLayer, MilestoneLayer, ObservableListDelegate, ProjectionIRIRelevamientoLayer}
 import UtilTransformers.PointTransformer
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
@@ -10,15 +10,16 @@ import scalafx.scene.layout.{BorderPane, Pane}
 import UtilTransformers.PointTransformer._
 import io.vmchura.vevial.elementdata.IRIElementData
 import io.vmchura.vevial.relevamiento.RelevamientoIRI
+import scalafx.scene.paint.Color
 import scalafx.scene.control.Button
 
 import scala.io.Codec
 import scala.reflect.io.File
 object HelloScalaFX extends JFXApp {
 
-  val fileXML = File("/home/vmchura/Documents/001.Projects/vevial/ejevialview/src/test/resources/tramo123.xml")
-  val fileCSV1 = new java.io.File("/home/vmchura/Documents/My Roughometer/Survey Results/2018-08-01/2018-08-01 10h02m32s Survey.csv")
-  val fileCSV2 = new java.io.File("/home/vmchura/Documents/My Roughometer/Survey Results/2018-08-01/2018-08-01 13h24m33s Survey.csv")
+  private val fileXML = File("/home/vmchura/Documents/001.Projects/vevial/ejevialview/src/test/resources/tramo123.xml")
+  private val fileCSV1 = new java.io.File("/home/vmchura/Documents/My Roughometer/Survey Results/2018-08-01/2018-08-01 10h02m32s Survey.csv")
+  private val fileCSV2 = new java.io.File("/home/vmchura/Documents/My Roughometer/Survey Results/2018-08-01/2018-08-01 13h24m33s Survey.csv")
 
   val arraySeqNodes: Array[ObservableBuffer[Node]] = {
     new LandXMLToEje(fileXML.reader(Codec("UTF-8"))).toEje match {
@@ -29,8 +30,8 @@ object HelloScalaFX extends JFXApp {
         offsetY() = eje.elements.head.in.point.y
         val relIRI1 = RelevamientoIRI(fileCSV1,cd => IRIElementData(cd)).sliceBy(eje.leftmostPoint.x,eje.rightmostPoint.x,eje.lowerPoint.y,eje.upperPoint.y)
         val relIRI2 = RelevamientoIRI(fileCSV2,cd => IRIElementData(cd)).sliceBy(eje.leftmostPoint.x,eje.rightmostPoint.x,eje.lowerPoint.y,eje.upperPoint.y)
-        val relevamientoIRI1 = new ProjectionIRIRelevamientoLayer(relIRI1,eje)
-        val relevamientoIRI2 = new ProjectionIRIRelevamientoLayer(relIRI2,eje)
+        val relevamientoIRI1 = new ProjectionIRIRelevamientoLayer(relIRI1,eje, Color.Blue)
+        val relevamientoIRI2 = new ProjectionIRIRelevamientoLayer(relIRI2,eje, Color.Magenta)
         val ejeLayer: EjeVialLayer = new EjeVialLayer()
         ejeLayer.setEje(eje)
         val milestonesLayer = new MilestoneLayer(eje)
@@ -54,7 +55,7 @@ object HelloScalaFX extends JFXApp {
 
   val layersMerged = new ObservableListDelegate(arraySeqNodes,panelMapa.children)
 
-  
+
   val lastPositionX = new ObjectProperty[Option[Double]](this,"lastPositionX",None)
   val lastPositionY = new ObjectProperty[Option[Double]](this,"lastPositionY",None)
   panelMapa.onMouseDragged = ae => {
