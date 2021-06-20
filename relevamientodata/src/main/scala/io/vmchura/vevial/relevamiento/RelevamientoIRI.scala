24db264acaafe17868d6808d77d83a8cfb6d7a0c
vmchura <vmchura@gmail.com>
2021-06-20 17:41:08 -0500

    Reformat

--- relevamientodata/src/main/scala/io/vmchura/vevial/relevamiento/RelevamientoIRI.scala
+++ relevamientodata/src/main/scala/io/vmchura/vevial/relevamiento/RelevamientoIRI.scala
@@ -1,94 +1,129 @@
 package io.vmchura.vevial.relevamiento
 import java.io.{File, FileReader}
 
-import io.vmchura.vevial.elementdata.{CrudeIRIData, DataWithPoint, IRIElementData, TElementData, TElementWithPoint, UPoint}
+import io.vmchura.vevial.elementdata.{
+  CrudeIRIData,
+  IRIElementData,
+  TElementWithPoint,
+  UPoint
+}
 import org.supercsv.io.CsvListReader
 import org.supercsv.prefs.CsvPreference
 
 import scala.collection.mutable.ListBuffer
 import scala.reflect.ClassTag
 
-
 /**
   * Representa una secuencia de datos referidos al IRI
   * no necesariamente del Roughmeter III
-  * @param elements
   */
-class RelevamientoIRI[T <: TElementWithPoint[T] : ClassTag](override val elements: Seq[T], val header: List[Array[String]], val interval: Int) extends TSimpleRelevamiento[T] {
+class RelevamientoIRI[T <: TElementWithPoint[T]: ClassTag](
+    override val elements: Seq[T],
+    val header: List[Array[String]],
+    val interval: Int
+) extends TSimpleRelevamiento[T] {
 
-
-  override def sliceBy(minX: Double, maxX: Double, minY: Double, maxY: Double): RelevamientoIRI[T] = {
-    new RelevamientoIRI(elements.filter{ e =>
-      e.point.exists { case UPoint(value, _) =>
-        minX <= value.x && value.x <= maxX &&
-          minY <= value.y && value.y <= maxY
-      }
-    },
-
-      header :+ Array(s"Solo considerando puntos en el rango Este-Oeste: [$minX:$maxX] Norte-Sur: [$minY:$maxY]"), interval)
+  override def sliceBy(
+      minX: Double,
+      maxX: Double,
+      minY: Double,
+      maxY: Double
+  ): RelevamientoIRI[T] = {
+    new RelevamientoIRI(
+      elements.filter { e =>
+        e.point.exists {
+          case UPoint(value, _) =>
+            minX <= value.x && value.x <= maxX &&
+              minY <= value.y && value.y <= maxY
+        }
+      },
+      header :+ Array(
+        s"Solo considerando puntos en el rango Este-Oeste: [$minX:$maxX] Norte-Sur: [$minY:$maxY]"
+      ),
+      interval
+    )
   }
 }
 
-object RelevamientoIRI{
-  def apply(elements: Seq[IRIElementData],header: List[Array[String]], interval: Int): RelevamientoIRI[IRIElementData] = new RelevamientoIRI(elements,header,interval)
+object RelevamientoIRI {
+  def apply(
+      elements: Seq[IRIElementData],
+      header: List[Array[String]],
+      interval: Int
+  ): RelevamientoIRI[IRIElementData] =
+    new RelevamientoIRI(elements, header, interval)
 
-  def apply[T <: TElementWithPoint[T] : ClassTag](source: File,builder: CrudeIRIData => T): RelevamientoIRI[T] = {
+  def apply[T <: TElementWithPoint[T]: ClassTag](
+      source: File,
+      builder: CrudeIRIData => T
+  ): RelevamientoIRI[T] = {
 
     import scala.jdk.CollectionConverters._
 
-    val reader = new CsvListReader(new FileReader(source),CsvPreference.STANDARD_PREFERENCE)
+    val reader = new CsvListReader(
+      new FileReader(source),
+      CsvPreference.STANDARD_PREFERENCE
+    )
     val registersBuffer = ListBuffer.empty[String]
     val headerBuffer = ListBuffer.empty[Array[String]]
     var continueRead = true
     var indx = 0
-    while(continueRead){
+    while (continueRead) {
       val lineRead = reader.read()
-      if(lineRead!=null){
-        val values=lineRead.asScala.toArray
-        if(indx>=25){
+      if (lineRead != null) {
+        val values = lineRead.asScala.toArray
+        if (indx >= 25) {
           registersBuffer += values.mkString(",")
-        }else{
-          headerBuffer += values.takeWhile(r => r!=null)
+        } else {
+          headerBuffer += values.takeWhile(r => r != null)
         }
 
-      }else{
+      } else {
         continueRead = false
       }
-      indx = indx+1
+      indx = indx + 1
     }
-
 
     //val (header, registers) = lines.getLines().toArray.splitAt(25)
     val registers = registersBuffer.toArray
 
     val header = headerBuffer.toList
-    val interval: Int = header.find(_.exists(_.contains("Processing interval:"))) match {
-      case Some(line) =>
-        val indxPI = line.indexWhere(_.contains("Processing interval:"))
-        val digitsOnly = line(indxPI+1).filter(_.isDigit)
-        if(digitsOnly.isEmpty)
-          throw new IllegalArgumentException("Incorrecto archivo, no es un .CSV válido/Not defined interval number")
-        else
-          digitsOnly.toInt
-      case None =>
-        throw new IllegalArgumentException("Incorrecto archivo, no es un .CSV válido/Not defined interval line")
-    }
+    val interval: Int =
+      header.find(_.exists(_.contains("Processing interval:"))) match {
+        case Some(line) =>
+          val indxPI = line.indexWhere(_.contains("Processing interval:"))
+          val digitsOnly = line(indxPI + 1).filter(_.isDigit)
+          if (digitsOnly.isEmpty)
+            throw new IllegalArgumentException(
+              "Incorrecto archivo, no es un .CSV válido/Not defined interval number"
+            )
+          else
+            digitsOnly.toInt
+        case None =>
+          throw new IllegalArgumentException(
+            "Incorrecto archivo, no es un .CSV válido/Not defined interval line"
+          )
+      }
     //val dataElements = registers.map(line => IRIElementData(new CrudeIRIData(line)))
     val dataElements = registers.map(line => builder(new CrudeIRIData(line)))
     val n = dataElements.length
     dataElements.zipWithIndex.foreach {
       case (iriElement, indx) =>
-
-
-        if (indx > 0) dataElements.update(indx, iriElement.withPrevElement(dataElements(indx - 1)))
-        if (indx + 1 < n) dataElements.update(indx, iriElement.withNextElement(dataElements(indx + 1)))
+        if (indx > 0)
+          dataElements.update(
+            indx,
+            iriElement.withPrevElement(dataElements(indx - 1))
+          )
+        if (indx + 1 < n)
+          dataElements.update(
+            indx,
+            iriElement.withNextElement(dataElements(indx + 1))
+          )
 
     }
     //lines.close()
-
+    reader.close()
     new RelevamientoIRI(dataElements, header, interval)
-
 
   }
 }
-
