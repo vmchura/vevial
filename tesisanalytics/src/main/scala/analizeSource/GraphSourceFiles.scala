f19a5597bce46306ecc910493e05faac9a878474
vmchura <vmchura@gmail.com>
2021-06-21 13:05:30 -0500

    Process Kml and CSV

--- tesisanalytics/src/main/scala/analizeSource/GraphSourceFiles.scala
+++ tesisanalytics/src/main/scala/analizeSource/GraphSourceFiles.scala
@@ -1,5 +1,7 @@
 package analizeSource
 
+import java.io.File
+
 case class GraphSourceFiles(
     nodes: Map[String, SourceFile],
     edges: Map[String, List[String]]
@@ -37,6 +39,67 @@ case class GraphSourceFiles(
 
     }
   }
+
+  def copyLargeNodes(destination: String): Unit = {
+    copyFilesWithProcess(
+      destination,
+      (sf, parentDirectory) => {
+
+        java.nio.file.Files.copy(
+          sf.inputFile.toPath,
+          new File(
+            parentDirectory,
+            sf.inputFile.getName
+          ).toPath
+        )
+
+      }
+    )
+  }
+  def copyLargeNodesAsKml(destination: String): Unit = {
+    copyFilesWithProcess(
+      destination,
+      (sf, parentDirectory) => {
+        sf.buildEje().map { eje =>
+          val fileResult = new File(
+            parentDirectory,
+            sf.inputFile.getName.replace(".csv", ".kml")
+          )
+          eje.exportKML(fileResult)
+        }
+
+      }
+    )
+  }
+
+  def copyFilesWithProcess(
+      destination: String,
+      process: (SourceFile, File) => Unit
+  ): Unit = {
+    val directoryResult = new File(destination)
+
+    require(
+      java.nio.file.Files.isDirectory(
+        directoryResult.toPath
+      ) || !java.nio.file.Files.exists(directoryResult.toPath),
+      "Result of destination should be a directory or not exists"
+    )
+    if (!java.nio.file.Files.exists(directoryResult.toPath))
+      java.nio.file.Files.createDirectory(directoryResult.toPath)
+
+    directoryResult.listFiles().foreach { f =>
+      if (java.nio.file.Files.isRegularFile(f.toPath)) {
+        java.nio.file.Files.delete(f.toPath)
+      }
+    }
+
+    largerNodes.foreach { sf =>
+      println(sf.inputFile.getName)
+      process(sf, directoryResult)
+
+    }
+  }
+
   override def toString: String =
     s"Larger: ${largerNodes.mkString("\n")} \n\nDuplex: ${duplexTramo.mkString("\n")}"
 }
